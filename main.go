92857009d6584c51d943184015c224bd8f209c48
cs50-romain <cs50.git.romain@gmail.com>
2023-08-16 19:34:53 -0400

    changed Data Struct to map within a map

--- main.go
+++ main.go
@@ -15,24 +15,19 @@ type CodeSnip struct {
 	filename string
 	content string
 }
-var languages []string
-var files = map[string][]string{}
+
+var languages map[string]map[string][]string
 
 func saveLang(lang string) {
-	count := 0
-	for _, val := range languages {
-		if lang == val {
-			count++
-		}
-	}
-
-	if count == 0 {
-		languages = append(languages, lang)
-	}
+	languages[lang] = make(map[string][]string)
 }
 
-func updateMap(fkey string, contents []string) {
-	files[fkey] = contents
+func updateMap(lkey string, fkey string, contents []string) {
+	languages[lkey][fkey] = contents
+}
+
+func deserializeJSON(){
+	
 }
 
 func serializeJSON(langs []string) {
@@ -59,11 +54,13 @@ func gatherContents(scanner *bufio.Scanner) []string{
 	return lines
 }
 
-func getContent(key string) []string{
-	return files[key]
+func getContent(lkey string, fkey string) []string{
+	return languages[lkey][fkey]
 }
 
 func main() {
+	languages = make(map[string]map[string][]string)
+
 	var lanFlag = flag.String("l", "Go", "Choose language to load/save code snippet")
 	var sFlag = flag.Bool("s", false, "Save file")
 	var fFlag = flag.String("f", "default.txt", "Specify filename")
@@ -71,12 +68,12 @@ func main() {
 
 	if *sFlag == true {
 		saveLang(*lanFlag)
-		fmt.Println("Input text:")
-		updateMap(*fFlag, gatherContents(bufio.NewScanner(os.Stdin)))
+		fmt.Println("Input text (q to quit):")
+		updateMap(*lanFlag, *fFlag, gatherContents(bufio.NewScanner(os.Stdin)))
 	}
 	
 	// Will most likely need changed to also include the language specified
-	content := getContent(*fFlag)
+	content := getContent(*lanFlag, *fFlag)
 
 	for _, lines := range content {
 		fmt.Println(lines)
